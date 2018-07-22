4e80ee7a04b7a625c2fb2cfc8cabdc1d165d15c4
Matt Wood <m.wood0904@gmail.com>
2018-07-22 07:36:15 -0500

    init

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	[SerializeField] float screenWidthInUnits = 16f;
+	[SerializeField] int min = 1;
+	[SerializeField] int max = 15;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+		Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+		paddlePosition.x = Mathf.Clamp(mousePositionInUnits, min, max);
+		transform.position = paddlePosition;
+	}
+}
